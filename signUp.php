48a87328db30f1332f1abafffc2d2451480660e5
nourahmaziad <nourahmaziad@gmail.com>
2023-05-08 03:09:57 +0300

    sign updates

--- signUp.php
+++ signUp.php
@@ -1,3 +1,65 @@
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+include 'includes/Security_inc.php';
+include 'includes/db_connect.php';
+	if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['age']) && isset($_POST['telnumber'])&& isset($_POST['numOfF'])&& isset($_POST['income'])&& isset($_POST['job'])&& isset($_POST['email'])&& isset($_POST['password'])){
+		$fname = mysqli_real_escape_string($databaseCon, $_POST['fname']);
+		$lname = mysqli_real_escape_string($databaseCon, $_POST['lname']);
+		$age = mysqli_real_escape_string($databaseCon, $_POST['age']);
+		$telnumber = mysqli_real_escape_string($databaseCon, $_POST['telnumber']);
+		$numOfF = mysqli_real_escape_string($databaseCon, $_POST['numOfF']);
+		$income = mysqli_real_escape_string($databaseCon, $_POST['income']);
+		$job = mysqli_real_escape_string($databaseCon, $_POST['job']);
+		$email = mysqli_real_escape_string($databaseCon, $_POST['email']);
+                $password= mysqli_real_escape_string($databaseCon, $_POST['password']);
+                
+                
+		$sql="SELECT * FROM `$TABLE_NAME` WHERE email_address='$email'";
+		$result = mysqli_query($databaseCon, $sql);
+                
+		
+		while($rows = mysqli_fetch_array($result)){
+			if( $email == $rows['email']){
+				$msg = '<h4 style="color:red;">This email entered exists</h4>';
+				break; 
+			}
+        }}
+		if (empty($msg)) {
+			$query = "INSERT INTO `student` (username, password, email, name) VALUES('$username', '".md5($password)."', '$email', '$fullname');";
+			$result = mysqli_query($databaseCon, $query) or die(mysqli_error($databaseCon));
+			
+			if ($result) {
+				$query = "SELECT * FROM `student` WHERE username='$username' and password='".md5($password)."'";
+				$result = mysqli_query($databaseCon, $query) or die(mysqli_error($databaseCon));
+				$rows = mysqli_num_rows($result);
+				
+				if ($rows == 1){
+					$row = mysqli_fetch_assoc($result);
+					$id = $row['id'];
+					$username = $row['username'];
+					$_SESSION['id'] = $id;
+					$_SESSION['username'] = $username;
+					$_SESSION['role']="student";
+					header("Location: student.php");
+				}
+				else{
+					$msg = '<h4 style="color:red;">Error in signup.</h4>';
+				}
+			} else {
+				$msg = '<h4 style="color:red;">Error in signup.</h4>';
+			}
+		}
+	}
+	else{
+		$msg = '<h4 style="color:red;">required data is missing!</h4>';
+	}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,12 +93,12 @@
     <div class="signup-container">
         <h1>SIGN UP</h1>
         <h3>It's Free and take only a minute!</h3>
-        <form name ="signupF"  >
+        <form name ="signupF"  method="post" action="signup.php">
             <p>first name:</p><input type="text" name="fname" placeholder=" first name" class="box"/>
             <p>last name:</p><input type="text" name="lname" placeholder=" last name" class="box"/>
             <p>age:</p><input type="text" name="age"placeholder=" age" class="box"/>
             <p>phone number:</p>
-            <input type="tel" name="tel-number" placeholder="phone number" class="box" > 
+            <input type="tel" name="telnumber" placeholder="phone number" class="box" > 
             <p>number of the family members:</p><input type="text" name="numOfF" class="box"/>
             <p>income:</p><input type="text"name="income" placeholder="income" class="box"/>
             <p>job:</p><input type="text"name="job" placeholder="job" class="box"/>
@@ -54,7 +116,7 @@
         <br>  <span>You have an Account already?</span><a href="login.php" >Log in now</a>
     </div>
    </div>
-   <!-- copyright ياارب -->
+   <!-- copyright  -->
    <div class="copyright">
     <p>&#169; YOUR HOME 2023.com</p>
   </div> 
