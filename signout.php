a661db0a3b1457e12cf5a306371a2404d8e94fbf
nourahmaziad <nourahmaziad@gmail.com>
2023-05-20 06:53:15 +0300

    دستروي

--- signout.php
+++ signout.php
@@ -2,17 +2,8 @@
 session_start();
 
 // Destroying All Sessions
-if ($_SESSION['role']=="home seeker"){
-	if(session_destroy()){
-		// Redirecting To Login Page
-		header("Location: login.php");
-	}
-}else{
-	if ($_SESSION['role']=="home owner"){
-		if(session_destroy()){
-			// Redirecting To Login Page
-			header("Location: login.php");
-		}	
-	}
-}
+session_destroy();
+// Redirecting To Login Page
+header("Location: login.php");
+
 ?>
